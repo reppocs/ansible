190208ba94cb9e1c3e3408a3e02acc819061b082
Corey Reppond <reppocs@gmail.com>
2022-11-10 16:23:37 -0500

    added sudos in script so the git clone wouldn't keep running as root

--- init.sh
+++ init.sh
@@ -4,20 +4,17 @@
 gituser="reppocs"
 gitrepo="ansible"
 
-# handle errors in a uniform manner                                                                              
-err() {                                                                                                          
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: ERROR: $*" >&2                                                       
+# handle errors in a uniform manner
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: ERROR: $*" >&2
   exit 1
 }
 
-# need sudo access
-[[ $(whoami) != "root" ]] && err "root permissions (sudo) required"
-
 # update and install required packages for ansible
-apt-add-repository -y ppa:ansible/ansible > /dev/null
+sudo apt-add-repository -y ppa:ansible/ansible > /dev/null
 DEBIAN_FRONTEND=noninteractive apt-get update -qq
-apt-get -y install curl git software-properties-common ansible -qq
-apt-get -y autoremove -qq
+sudo apt-get -y install curl git software-properties-common ansible -qq
+sudo apt-get -y autoremove -qq
 
 # get the ansible repo
 git clone https://github.com/"$gituser"/"$gitrepo".git && cd "$gitrepo" || err "cd failed"
