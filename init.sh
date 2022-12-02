2e6ed4cab1f95d4f5764498b3d280cf6fe5c9fde
Corey Reppond <reppocs@gmail.com>
2022-12-02 10:46:13 -0500

    removed unnecessary stuff... tried to be a bit too clever

--- init.sh
+++ init.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
-# git vars
-gituser="reppocs"
-gitrepo="ansible"
-
-# handle errors in a uniform manner
-err() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: ERROR: $*" >&2
-  exit 1
-}
-
-# update and install required packages for ansible
+# update and install required packages for latest ansible
 sudo apt-add-repository -y ppa:ansible/ansible > /dev/null
 DEBIAN_FRONTEND=noninteractive sudo apt-get update -qq
 sudo apt-get -y install curl git software-properties-common ansible -qq
 sudo apt-get -y autoremove -qq
 
-# get the ansible repo
-git clone https://github.com/"$gituser"/"$gitrepo".git && cd "$gitrepo" || err "cd failed"
+# clone the repo
+git clone https://github.com/reppocs/ansible.git
