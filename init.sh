48374ed011974fd4d9863e52f287f942e1d5585d
Corey Reppond <reppocs@gmail.com>
2022-12-27 16:18:56 -0500

    added ansible-lint package

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 # update and install required packages for latest ansible
 sudo apt-add-repository -y ppa:ansible/ansible > /dev/null
 DEBIAN_FRONTEND=noninteractive sudo apt-get update -qq
-sudo apt-get -y install curl git software-properties-common ansible -qq
+sudo apt-get -y install curl git software-properties-common ansible ansible-lint qq
 sudo apt-get -y autoremove -qq
 
 # clone the repo
